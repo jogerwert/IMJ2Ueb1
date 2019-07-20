4b0c684b515a2caca2a122587b3ddb90b3bf0e62
jogerwert <jogerwert@web.de>
2019-07-20 22:11:30 +0200

    Initial Project created with Spring Boot

---
+++ Kaffeemaschine/src/main/java/de/stl/saar/internetentw2/uebungen/Kaffeemaschine/KaffeemaschineApplication.java
@@ -0,0 +1,13 @@
+package de.stl.saar.internetentw2.uebungen.Kaffeemaschine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaffeemaschineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaffeemaschineApplication.class, args);
+	}
+
+}
